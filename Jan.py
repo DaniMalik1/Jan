a4c799748f133ff4125ad51fa469f9e38f352324
Dani Malik <112042251+DaniMalik1@users.noreply.github.com>
2025-07-30 15:46:06 +0500

    Update Jan.py

--- Jan.py
+++ Jan.py
@@ -1,1 +1,22 @@
-import zid_enc
+#####apni 
+ #coding=utf-8
+import os, sys, platform
+ 
+os.system('git pull')
+ 
+try:
+    if sys.argv[1]=='update':
+        os.system('git pull')
+except:
+    pass
+ 
+ 
+bit = platform.architecture()[0]
+if bit == '64bit':
+    
+        import zi_enc
+    
+ 
+elif bit == '32bit':
+    
+        import run_enc
